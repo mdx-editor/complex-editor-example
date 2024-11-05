3d770370e472a9a7bd19e6782b039ba2e6883efe
Petyo Ivanov <underlog@gmail.com>
2024-11-05 09:59:01 +0200

    External control on view mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import {
   DiffSourceToggleWrapper,
   headingsPlugin,
   MDXEditor,
+  realmPlugin,
   toolbarPlugin,
   UndoRedo,
+  viewMode$,
 } from "@mdxeditor/editor";
 
 import "@mdxeditor/editor/style.css";
@@ -25,7 +27,18 @@ const markdown = `
   A paragraph
 `;
 
+const externalViewModePlugin = realmPlugin<{ sourceMode: boolean }>({
+  init(realm, params) {
+    realm.pub(viewMode$, params?.sourceMode ? "source" : "rich-text");
+  },
+  update(realm, params) {
+    realm.pub(viewMode$, params?.sourceMode ? "source" : "rich-text");
+  },
+});
+
 function App() {
+  const [sourceMode, setSourceMode] = useState(false);
+  const inDarkModeRef = useRef(false);
   const codeMirrorViewRef = useRef<EditorView | null>(null);
 
   const [editorTheme] = useState(() => {
@@ -37,31 +50,63 @@ function App() {
       <label>
         <input
           type="checkbox"
-          onChange={(e) =>
+          onChange={(e) => {
+            inDarkModeRef.current = e.target.checked;
             codeMirrorViewRef.current?.dispatch({
               effects: editorTheme.reconfigure(
-                e.target.checked ? basicDark : basicLight,
+                inDarkModeRef.current ? basicDark : basicLight,
               ),
-            })
-          }
+            });
+          }}
         />{" "}
         Dark mode for source view.
       </label>
+
+      <label>
+        <input
+          type="checkbox"
+          checked={sourceMode}
+          onChange={() => setSourceMode((prev) => !prev)}
+        />{" "}
+        Source mode
+      </label>
+
       <MDXEditor
         contentEditableClassName="my-editor"
         markdown={markdown}
         plugins={[
           headingsPlugin(),
           htmlElementsPlugin(),
+
+          // this enables dark mode when switching to source mode
+          realmPlugin({
+            init: (r) => {
+              r.sub(viewMode$, (mode) => {
+                if (mode === "source") {
+                  // delay so that the ref is present.
+                  setTimeout(() => {
+                    codeMirrorViewRef.current?.dispatch({
+                      effects: editorTheme.reconfigure(
+                        inDarkModeRef.current ? basicDark : basicLight,
+                      ),
+                    });
+                  });
+                }
+              });
+            },
+          })(),
+
           diffSourcePlugin({
             codeMirrorExtensions: [
-              // this will work for code blocks in the rich mode, too
               editorTheme.of(basicLight),
               EditorView.updateListener.of((update) => {
                 codeMirrorViewRef.current = update.view;
               }),
             ],
           }),
+
+          externalViewModePlugin({ sourceMode }),
+
           toolbarPlugin({
             toolbarContents: () => (
               <DiffSourceToggleWrapper>
